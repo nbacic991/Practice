d08aa9fcd70fc950ca2e3a2ab87a973afa294500
Nemanja Bacic <nbacic@ymail.com>
2018-01-24 16:03:22 +0100

    Renamed database and used source to run it.

---
+++ databases/nemanja.sql
@@ -0,0 +1,17 @@
+# INSERT INTO actors (first_name, last_name) VALUES
+#   ('Ben','Stiller'),
+#   ('Johny','Depp'),
+#   ('Johny','Depp'),
+#   ('Leonardo','DiCaprio'),
+#   ('Denzel','Washington'),
+#   ('Robert','Downey Jr.'),
+#   ('Matt','Damon'),
+#   ('Dwayne','Johnson'),
+#   ('Robert','De Niro'),
+#   ('Tom','Hanks'),
+#   ('Tom','Cruise');
+
+
+# DELETE FROM actors;
+
+SELECT * FROM actors;
