3e7f623053875fdff8bb513bc4ac92a0f42dc708
Nemanja Bacic <nbacic@ymail.com>
2018-01-29 14:05:47 +0100

    Style fix.

--- webroot/PHP practice/js.php
+++ webroot/PHP practice/js.php
@@ -5,11 +5,17 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<link rel="stylesheet" href="../style.css" type="text/css">
     <title>JavaScript and jQuery</title>
 </head>
 <body>
-<div class="java-script">
-	<h1>This is my JavaScript file for practice !</h1>
-</div>
+<section class="java-script">
+	<div class="title-block">
+		<h1>This is my JavaScript file for practice !</h1>
+	</div>
+	<div class="practice-block">
+
+	</div>
+</section>
 </body>
 </html>
